09a6d26b10fb28c25278df858c3543622d0e3e57
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2019-11-23 08:46:32 -0500

    se añadio el boton para sucribirse

--- src/app/modules/view-tutoring/pages/tutoring-session/tutoring-session.component.ts
+++ src/app/modules/view-tutoring/pages/tutoring-session/tutoring-session.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ViewTutoringService } from '../../services';
 import { TutoringSessionResponse } from 'src/app/shared/dtos/Output/TutoringSessionResponse';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { AuthenticationService } from 'src/app/core';
 
 @Component({
   selector: 'app-tutoring-session',
@@ -17,7 +18,8 @@ export class TutoringSessionComponent implements OnInit {
 	constructor(
 		private viewTutoringService: ViewTutoringService,
 		private route: ActivatedRoute,
-		private router: Router
+		private router: Router,
+		private authenticationService: AuthenticationService
 		) { }
 
 	async ngOnInit() {
@@ -30,6 +32,12 @@ export class TutoringSessionComponent implements OnInit {
 		if	(this.tutoringSession.studentCount == null){
 			this.tutoringSession.studentCount = 0;
 		}
+	}
+
+	async reserve(e){
+		await this.viewTutoringService.reserveTutoringSession(e,this.authenticationService.userValue.id).then( ()=>{
+			console.log("se realizo con exito")
+		});
 	}
 
 }
--- src/app/modules/view-tutoring/services/view-tutoring/view-tutoring.service.ts
+++ src/app/modules/view-tutoring/services/view-tutoring/view-tutoring.service.ts
@@ -1,36 +1,45 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { TutoringOfferResponse } from 'src/app/shared/dtos/Output/TutoringOfferResponse';
-import { environment } from 'src/environments/environment';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { TutoringOfferResponse } from "src/app/shared/dtos/Output/TutoringOfferResponse";
+import { environment } from "src/environments/environment";
 
 @Injectable({
-	providedIn: 'root'
+	providedIn: "root"
 })
 export class ViewTutoringService {
-
 	tutoringOffer: TutoringOfferResponse;
 
-	constructor(private httpClient: HttpClient) { }
+	constructor(private httpClient: HttpClient) {}
 
 	private async checkData(tutoringId: number) {
-		if (this.tutoringOffer == null || this.tutoringOffer.tutoringOfferId !== tutoringId ) {
+		if (
+			this.tutoringOffer == null ||
+			this.tutoringOffer.tutoringOfferId !== tutoringId
+		) {
 			this.tutoringOffer = await this.httpClient
 				.get<TutoringOfferResponse>(
 					`${environment.apiUrl}/tutoringoffers/searchById/${tutoringId}`
-				).toPromise<TutoringOfferResponse>();
+				)
+				.toPromise<TutoringOfferResponse>();
 		}
 	}
 
-	async getTutoringOffer(tutoringId: number){
+	async getTutoringOffer(tutoringId: number) {
 		await this.checkData(tutoringId);
 		return this.tutoringOffer;
 	}
 
-	async getTutoringSession(tutoringId: number, sessionId: number){
+	async getTutoringSession(tutoringId: number, sessionId: number) {
 		await this.checkData(tutoringId);
 
-		for(let i=0; i<this.tutoringOffer.tutoringSessionResponses.length; ++i) {
-			if (this.tutoringOffer.tutoringSessionResponses[i].id === sessionId) {
+		for (
+			let i = 0;
+			i < this.tutoringOffer.tutoringSessionResponses.length;
+			++i
+		) {
+			if (
+				this.tutoringOffer.tutoringSessionResponses[i].id === sessionId
+			) {
 				return this.tutoringOffer.tutoringSessionResponses[i];
 			}
 		}
@@ -40,10 +49,13 @@ export class ViewTutoringService {
 		return this.tutoringOffer.tutor;
 	}
 
-
-
-
-
-
-
+	async reserveTutoringSession(idSession, idUser) {
+		let CreateTutoringSession = {
+			"tutoring_session_id": idSession,
+			"student_id": idUser
+		};
+		const uri = `${environment.apiUrl}/TutoringSessionStudent`;
+		await this.httpClient
+			.post<any>(uri, CreateTutoringSession).toPromise<any>();
+	}
 }
